d29bc3d009a39a74bbbb82defb9e507945b7d3de
@BushidoZr <h.zerouali04@gmail.com>
2024-01-19 01:13:24 +0100

    far

--- App.js
+++ App.js
@@ -8,29 +8,29 @@ import SignUp from './pages/signUp';
 
 const App = () => {
   return (
-    // <View style={styles.container}>
-    //   {/* <Text>Welcome to My React Native App</Text> */}
-    //   <Info /> 
-    // </View>
-     <NavigationContainer style={{ backgroundColor: '#212832'}}>
-        <Stack.Navigator initialRouteName="Login">
+    <View style={styles.container}>
+      {/* <Text>Welcome to My React Native App</Text> */}
+      <Login /> 
+    </View>
+    //  <NavigationContainer style={{ backgroundColor: '#212832'}}>
+    //     <Stack.Navigator initialRouteName="Login">
        
-            <Stack.Screen name="Login"
-                            component={Login}
-                            options={{headerStyle: {backgroundColor: '#212832',},headerTintColor: '#fff', }}/>
-              <Stack.Screen name="SignUp"
-                            component={SignUp}
-                            options={{headerStyle: {backgroundColor: '#212832',},headerTintColor: '#fff', }}/>
-              <Stack.Screen name="Profil"
-                            component={Acceuil}
-                            options={{headerStyle: {backgroundColor: '#212832',},headerTintColor: '#fff', }}/>
-              <Stack.Screen name="Homepage"
-                            component={Info}
-                            options={{ headerStyle: { backgroundColor: '#212832' }, headerTintColor: '#fff' }}
-                            >      
-              </Stack.Screen>
-          </Stack.Navigator>
-      </NavigationContainer>
+    //         <Stack.Screen name="Login"
+    //                         component={Login}
+    //                         options={{headerStyle: {backgroundColor: '#212832',},headerTintColor: '#fff', }}/>
+    //           <Stack.Screen name="SignUp"
+    //                         component={SignUp}
+    //                         options={{headerStyle: {backgroundColor: '#212832',},headerTintColor: '#fff', }}/>
+    //           <Stack.Screen name="Profil"
+    //                         component={Acceuil}
+    //                         options={{headerStyle: {backgroundColor: '#212832',},headerTintColor: '#fff', }}/>
+    //           <Stack.Screen name="Homepage"
+    //                         component={Info}
+    //                         options={{ headerStyle: { backgroundColor: '#212832' }, headerTintColor: '#fff' }}
+    //                         >      
+    //           </Stack.Screen>
+    //       </Stack.Navigator>
+    //   </NavigationContainer>
   );
 }
 
